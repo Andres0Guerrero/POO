cb1acdbda5e67a4696c61c298648821da991deaf
Andres0Guerrero <aguerrerome@unal.edu.co>
2026-02-11 20:36:22 -0500

    Update Ejercicio_No4.py
    se arregla un error de fracciones

--- Ejercicio_No4.py
+++ Ejercicio_No4.py
@@ -3,7 +3,7 @@
 # la edad de la mama es la suma de las tres edades
 edad_juan = int(input(" Hola Juan, ¿cuantos años tienes? : "))
 edad_alberto = int(edad_juan*2/3) 
-edad_ana = int(edad_juan*3/4)
+edad_ana = int(edad_juan*4/3)
 edad_mamá = int(edad_juan + edad_alberto + edad_ana)
 print (f"la edad de juan es : {edad_juan} años"); print (f"la edad de alberto es : {edad_alberto} años"); print (f"la edad de ana es : {edad_ana} años");
 print (f"la edad de la mamá es : {edad_mamá} años");
