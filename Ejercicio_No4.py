5ee3ff3b606678fdd6735cdf1fc0559e2c2e9a70
Andres0Guerrero <aguerrerome@unal.edu.co>
2026-02-11 20:40:22 -0500

    Update Ejercicio_No4.py

--- Ejercicio_No4.py
+++ Ejercicio_No4.py
@@ -1,5 +1,5 @@
 # la mama de juan tiene 3 hijos #preguntarle la edad a juan
-#Alberto tendrá 2/3 de la edad de juan #Ana debe tener 3/4 de la edad de juan
+#Alberto tendrá 2/3 de la edad de juan #Ana debe tener 4/3 de la edad de juan
 # la edad de la mama es la suma de las tres edades
 edad_juan = int(input(" Hola Juan, ¿cuantos años tienes? : "))
 edad_alberto = int(edad_juan*2/3) 
